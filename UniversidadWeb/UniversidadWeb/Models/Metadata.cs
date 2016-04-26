b2ae79281d98bac5b531e4574e7dca8280d07591
Phillipman313 <phillipman313@gmail.com>
2016-04-25 22:22:48 -0600

    Periodos

---
+++ UniversidadWeb/UniversidadWeb/Models/Metadata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UniversidadWeb.Models
+{
+    public class AnyoMetadata
+    {
+        [Display(Name = "Año")]
+        public string Anyo1;
+    }
+}
