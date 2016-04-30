c824f43335a084f1c609e2e6ca31d008fd6d5c38
Phillipman313 <phillipman313@gmail.com>
2016-04-30 17:15:32 -0600

    Horarios

--- UniversidadWeb/UniversidadWeb/Models/ClasesParciales.cs
+++ UniversidadWeb/UniversidadWeb/Models/ClasesParciales.cs
@@ -19,13 +19,24 @@ namespace UniversidadWeb.Models
 
     public partial class CursoLectivo
     {
-        private Horario horas;
+        private string horas;
 
-        public Horario Horas
+        public string Horas
         {
             get
             {
-                return Horario.Where(x => x.Id_CursoLectivoLF == Id_CursoLectivo).First();
+                List<Horario> dias = Horario.Where(x => x.Id_CursoLectivoLF == Id_CursoLectivo).ToList<Horario>();
+                int largo = dias.Count - 1;
+                string horas = "";
+                Horario parte = null;
+                for (int i = 0; i < largo; i++)
+                {
+                    parte = dias.ElementAt(i);
+                    horas += parte.HoraInicio + " - " + parte.HoraFin + " " + parte.DiaSemana.Codigo + "\n";
+                }
+                parte = dias.ElementAt(largo);
+                horas += parte.HoraInicio + " - " + parte.HoraFin + " " + parte.DiaSemana.Codigo;
+                return horas;
             }
         }
     }
